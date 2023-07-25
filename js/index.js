aac50f2949832585ceeee4e04c4fa8746a389fc9
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2023-07-25 23:33:05 +0530

    add relevant skills and tools

--- js/index.js
+++ js/index.js
@@ -8,12 +8,39 @@ function loadLinks(profileLinks) {
     }
     $('#tabs-links').append(profileLinksInnerHTML);
 }
+
 function onBodyLoad() {
     $('div.progress').css('display', 'none');
     $('div.content').css('display', 'block');
 }
 
-
+function loadSkills(skills) {
+    let i = 0;
+    var skillsInnerHTML = '';
+    while (i < skills.length) {
+        var row = '<div class="row">';
+        for (let j = i; j < i + 12 && j < skills.length; j++) {
+            if (skills[j][1].startsWith("devicon")) {
+                var skill = `
+				<div class="col m1 s3 skill">					
+					<span class="icon"><i class="${skills[j][1]}"></i></span>				
+					<span>${skills[j][0]}</span>
+				</div>`;
+            } else {
+                var skill = `
+				<div class="col m1 s3 skill">					
+					<span class="icon"><img src="${skills[j][1]}"/></span>				
+					<span>${skills[j][0]}</span>
+				</div>`;
+            }
+            row += skill;
+        }
+        row += '</div>';
+        skillsInnerHTML += row;
+        i += 12;
+    }
+    $('#skills').html(`<div class="row section"><h4>Skills</h4>${skillsInnerHTML}</div>`);
+}
 
 $.get("js/profile.json",
     function(data, status) {
@@ -28,6 +55,7 @@ $.get("js/profile.json",
         $('#contact').html(`<span>${pInfo.mob}</span></br><span><a href="mailto:${pInfo.email}">${pInfo.email}</a></span>`);
         $('#summary').html(profile.summary);
         loadLinks(profile.profileLinks);
+        loadSkills(profile.skills);
         console.log('body loaded calling');
         onBodyLoad();
     });
